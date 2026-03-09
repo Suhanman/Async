0ff9d7128fab67214cd98c83d1edb7bd21e249bd
Suhan <victor1919@naver.com>
2026-03-10 02:49:42 +0900

    transaction

--- RabbitMQ/HelloMessageQueue/src/main/java/com/example/hellomessagequeue/step7/RetryConfig.java
+++ RabbitMQ/HelloMessageQueue/src/main/java/com/example/hellomessagequeue/step7/RetryConfig.java
@@ -1,30 +1,30 @@
-package com.example.hellomessagequeue.step7;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.support.RetryTemplate;
-import org.springframework.util.backoff.FixedBackOff;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-
-
-@Configuration
-public class RetryConfig {
-
-    @Bean
-    public RetryTemplate retryTemplate(){
-        RetryTemplate retryTemplate = new RetryTemplate();
-        // 재시도 정책 설정 : 최대 3번의 시도
-        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
-        retryPolicy.setMaxAttempts(3);
-        // 백오프 정책 설정 : 재시도 간격 1초
-        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
-        backOffPolicy.setBackOffPeriod(1000L);
-        retryTemplate.setRetryPolicy(retryPolicy);
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-
-        return retryTemplate;
-    }
-}
+//package com.example.hellomessagequeue.step7;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import org.springframework.retry.backoff.FixedBackOffPolicy;
+//import org.springframework.retry.support.RetryTemplate;
+//import org.springframework.util.backoff.FixedBackOff;
+//import org.springframework.retry.policy.SimpleRetryPolicy;
+//
+//
+//@Configuration
+//public class RetryConfig {
+//
+//    @Bean
+//    public RetryTemplate retryTemplate(){
+//        RetryTemplate retryTemplate = new RetryTemplate();
+//        // 재시도 정책 설정 : 최대 3번의 시도
+//        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+//        retryPolicy.setMaxAttempts(3);
+//        // 백오프 정책 설정 : 재시도 간격 1초
+//        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+//        backOffPolicy.setBackOffPeriod(1000L);
+//        retryTemplate.setRetryPolicy(retryPolicy);
+//        retryTemplate.setBackOffPolicy(backOffPolicy);
+//
+//        return retryTemplate;
+//    }
+//}
---
+++ RabbitMQ/HelloMessageQueue/src/main/java/com/example/hellomessagequeue/step8/StockRepository.java
@@ -0,0 +1,14 @@
+package com.example.hellomessagequeue.step8;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
+
+
+}
+
+
+
+
+
+
